20d40b5fb7c115b254e53dadc02edbe3448be0c9
Evgeniya-Mankovskaya <59099458+Evgeniya-Mankovskaya@users.noreply.github.com>
2021-07-04 11:58:56 +0000

    Deploying to gh-pages from @ Evgeniya-Mankovskaya/pg-16-project-1@fdf7f0b30e5658e5750b412d500567e0601a5006 🚀

--- src.7428e530.js
+++ src.7428e530.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/contacts/contacts-mob.jpg":[["contacts-mob.f00a06ca.jpg","YfDV"],"YfDV"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/pg-16-project-1/src.b06f4399.js.map
+//# sourceMappingURL=/pg-16-project-1/src.7428e530.js.map
