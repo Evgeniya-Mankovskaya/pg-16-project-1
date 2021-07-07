6f424c28ea6736a545f99809586c7cb4e3c72a7c
Yevheniia Mankivska <evgeniyamankovskaya@gmail.com>
2021-07-07 22:29:58 +0300

    slick-slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,1 +1,14 @@
-$('.reviews__slider').slick();
+$('.reviews__slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.reviews__slider-2',
+});
+$('.reviews__slider-2').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.reviews__slider',
+  centerMode: true,
+  focusOnSelect: true,
+});
